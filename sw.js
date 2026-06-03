dbb8ad9d45ea207e01ae9c9d4a67fcb0f8a64a22
Fujisawa53 <k.ohata1979@gmail.com>
2026-06-03 21:40:13 +0900

    Millisecond stopwatch (DSEG7 LCD, offline PWA)

---
+++ sw.js
@@ -0,0 +1,22 @@
+// 単純な cache-first Service Worker（オフライン起動用）
+const CACHE = 'stopwatch-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request))
+  );
+});
