7af14eeef885312023993673d6bbd4399cc6df09
Fujisawa53 <k.ohata1979@gmail.com>
2026-06-05 17:06:19 +0900

    fps label: uppercase + no space (60FPS), SW cache v8

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 単純な cache-first Service Worker（オフライン起動用）
-const CACHE = 'stopwatch-v7';
+const CACHE = 'stopwatch-v8';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (e) => {
