288909a807f6c4d554ff2d0e30635006e4501537
Fujisawa53 <k.ohata1979@gmail.com>
2026-06-05 21:25:25 +0900

    Center display in both orientations (drop translateY); default color orange, SW cache v19

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 単純な cache-first Service Worker（オフライン起動用）
-const CACHE = 'mscounter-v18';
+const CACHE = 'mscounter-v19';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
   './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'];
 
